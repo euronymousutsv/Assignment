724bb5d37e158ee992e1c20bc77ee72fca48a104
utsav <utsavkhatri7@gmail.com>
2024-03-31 13:13:04 +1100

    first commit

---
+++ components/AuthenticationForm.js
@@ -0,0 +1,45 @@
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+
+export function AuthenticationForm(props) {
+    return (
+        <View style={styles.form}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text>Email</Text>
+            <TextInput style={styles.input} />
+            <Text>Password</Text>
+            <TextInput style={styles.input} secureTextEntry={true}/>
+            <Pressable style={styles.button}>
+                <Text style={styles.buttonText}>{props.action}</Text>
+            </Pressable>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    form: {
+        backgroundColor: "white",
+        marginHorizontal: 20,
+        padding: 20,
+        borderRadius:10,
+
+
+    },
+    title: {
+        fontSize: 20
+
+    },
+    input: {
+        borderBottomColor: "#333333",
+        borderBottomWidth: 2,
+    },
+    button:{
+        marginTop:30,
+        backgroundColor: "darkblue",
+        padding: 10,
+
+    },
+    buttonText:{
+        color:"white",
+        textAlign:"center",
+    }
+
+})
